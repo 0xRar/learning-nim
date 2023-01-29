58fb8abc966ae41ea82d0e2bd5cfe61f4a5e8fd6
0xRar <33517160+0xRar@users.noreply.github.com>
2023-01-29 11:14:33 +0300

    Create nimCalc.nim

---
+++ nimCalc.nim
@@ -0,0 +1,27 @@
+import strutils
+
+# a calculator written in nim ! by 0xRar 😁
+
+echo "Enter the operation(+, -, *, /):"
+let op = readLine(stdin)
+
+echo "Enter the first number:"
+let fnum = readLine(stdin).parseInt()
+
+echo "Enter the second number:"
+let snum = readLine(stdin).parseInt()
+
+if op == "+":
+    echo fnum, " + ", snum, " = ", fnum + snum
+
+elif op == "-":
+    echo fnum, " - ", snum, " = ", fnum - snum
+
+elif op == "*":
+    echo fnum, " * ", snum, " = ", fnum * snum
+
+elif op == "/":
+    echo fnum, " / ", snum, " = ", fnum / snum
+
+else:
+    echo "😐Sorry i only accept (+, -, *, /)"
